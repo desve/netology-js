539b992069c32d58f8aa9dda6039f9bcc302e593
desve <2901243@mail.ru>
2017-09-12 11:05:50 +0300

    first commit

--- 1.1/trains.js
+++ 1.1/trains.js
@@ -5,5 +5,6 @@ distance = 1056, time, distanceFromMsc;
 
 time = distance / (trainMscSpeed + trainSmrSpeed);
 distanceFromMsc = trainMscSpeed * time;
+
 console.log(`Поезда встретятся через ${time} часов`);
 console.log(`Встреча произойдет в ${distanceFromMsc} км от Москвы`);
